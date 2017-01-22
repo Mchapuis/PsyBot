0decbc400e4ce3fe3885596bc8209377a059c9ab
Richard <rch@rogers.com>
2017-01-21 20:47:27 -0500

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -1,11 +1,11 @@
 #Consumer Key (API Key)
-C_KEY = 'YhaaeQNLM0L3OBLQp6ILXwTiE'
+C_KEY = ''
 
 # Consumer Secret (API Secret)
-C_SECRET = 'd1vM8XXJ5ucoZq1A8AztF24Ob2FsbQOza6JZp7Ho1ySA9HwUza'
+C_SECRET = ''
 
 # Access Token
-A_TOKEN = '822889980557217794-Wuzsqo1zoMDk8Vf9iaS5YeefSCsuNgI'
+A_TOKEN = ''
 
 # Access Token Secret
-A_TOKEN_SECRET = 'zoNsrmm4vfgRJNmlX2UlPaneIrE2ajmEgKIq4uU5iu57n'
+A_TOKEN_SECRET = ''
